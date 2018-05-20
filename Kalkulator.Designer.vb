0fc5da2717d430a41d91e1a883d446e7d744b55a
Hafiznaufalr <hafiz.nr5758@gmail.com>
2018-05-20 21:57:48 +0700

    commit

---
+++ Kalkulator.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtHasil = New System.Windows.Forms.TextBox()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnBagi = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btnKali = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnKurang = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnCE = New System.Windows.Forms.Button()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.btnHasil = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtHasil
+        '
+        Me.txtHasil.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHasil.Location = New System.Drawing.Point(29, 9)
+        Me.txtHasil.Multiline = True
+        Me.txtHasil.Name = "txtHasil"
+        Me.txtHasil.Size = New System.Drawing.Size(231, 38)
+        Me.txtHasil.TabIndex = 0
+        Me.txtHasil.Text = "0"
+        Me.txtHasil.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(29, 65)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(38, 34)
+        Me.btn7.TabIndex = 4
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(91, 65)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(38, 34)
+        Me.btn8.TabIndex = 5
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(155, 65)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(38, 34)
+        Me.btn9.TabIndex = 6
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnBagi
+        '
+        Me.btnBagi.ForeColor = System.Drawing.Color.Red
+        Me.btnBagi.Location = New System.Drawing.Point(222, 65)
+        Me.btnBagi.Name = "btnBagi"
+        Me.btnBagi.Size = New System.Drawing.Size(38, 34)
+        Me.btnBagi.TabIndex = 7
+        Me.btnBagi.Text = "/"
+        Me.btnBagi.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(29, 117)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(38, 34)
+        Me.btn4.TabIndex = 9
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(91, 117)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(38, 34)
+        Me.btn5.TabIndex = 10
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(155, 117)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(38, 34)
+        Me.btn6.TabIndex = 11
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btnKali
+        '
+        Me.btnKali.ForeColor = System.Drawing.Color.Red
+        Me.btnKali.Location = New System.Drawing.Point(222, 117)
+        Me.btnKali.Name = "btnKali"
+        Me.btnKali.Size = New System.Drawing.Size(38, 34)
+        Me.btnKali.TabIndex = 12
+        Me.btnKali.Text = "*"
+        Me.btnKali.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(29, 169)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(38, 34)
+        Me.btn1.TabIndex = 14
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(91, 169)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(38, 34)
+        Me.btn2.TabIndex = 15
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(155, 169)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(38, 34)
+        Me.btn3.TabIndex = 16
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btnKurang
+        '
+        Me.btnKurang.ForeColor = System.Drawing.Color.Red
+        Me.btnKurang.Location = New System.Drawing.Point(222, 169)
+        Me.btnKurang.Name = "btnKurang"
+        Me.btnKurang.Size = New System.Drawing.Size(38, 34)
+        Me.btnKurang.TabIndex = 17
+        Me.btnKurang.Text = "-"
+        Me.btnKurang.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(29, 221)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(38, 34)
+        Me.btn0.TabIndex = 19
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnCE
+        '
+        Me.btnCE.Location = New System.Drawing.Point(91, 221)
+        Me.btnCE.Name = "btnCE"
+        Me.btnCE.Size = New System.Drawing.Size(38, 34)
+        Me.btnCE.TabIndex = 21
+        Me.btnCE.Text = "CE"
+        Me.btnCE.UseVisualStyleBackColor = True
+        '
+        'btnTambah
+        '
+        Me.btnTambah.ForeColor = System.Drawing.Color.Red
+        Me.btnTambah.Location = New System.Drawing.Point(222, 221)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(38, 34)
+        Me.btnTambah.TabIndex = 22
+        Me.btnTambah.Text = "+"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'btnHasil
+        '
+        Me.btnHasil.ForeColor = System.Drawing.Color.Blue
+        Me.btnHasil.Location = New System.Drawing.Point(155, 221)
+        Me.btnHasil.Name = "btnHasil"
+        Me.btnHasil.Size = New System.Drawing.Size(38, 34)
+        Me.btnHasil.TabIndex = 23
+        Me.btnHasil.Text = "="
+        Me.btnHasil.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(280, 282)
+        Me.Controls.Add(Me.btnHasil)
+        Me.Controls.Add(Me.btnTambah)
+        Me.Controls.Add(Me.btnCE)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnKurang)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnKali)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnBagi)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.txtHasil)
+        Me.Name = "Form1"
+        Me.Text = "Kalkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtHasil As System.Windows.Forms.TextBox
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btnBagi As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btnKali As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btnKurang As System.Windows.Forms.Button
+    Friend WithEvents btn0 As System.Windows.Forms.Button
+    Friend WithEvents btnCE As System.Windows.Forms.Button
+    Friend WithEvents btnTambah As System.Windows.Forms.Button
+    Friend WithEvents btnHasil As System.Windows.Forms.Button
+
+End Class
---
+++ Kalkulator.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+
+    Dim FirstNumber As Single
+    Dim SecondNumber As Single
+    Dim AnswerNumber As Single
+    Dim ArithmeticProcess As String
+
+    Private Sub btn0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn0.Click
+        txtHasil.Text = txtHasil.Text & 0
+    End Sub
+
+    Private Sub btn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn1.Click
+        txtHasil.Text = txtHasil.Text & 1
+    End Sub
+
+    Private Sub btn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn2.Click
+        txtHasil.Text = txtHasil.Text & 2
+    End Sub
+
+    Private Sub btn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn3.Click
+        txtHasil.Text = txtHasil.Text & 3
+    End Sub
+
+    Private Sub btn4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn4.Click
+        txtHasil.Text = txtHasil.Text & 4
+    End Sub
+
+    Private Sub btn5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn5.Click
+        txtHasil.Text = txtHasil.Text & 5
+    End Sub
+
+    Private Sub btn6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn6.Click
+        txtHasil.Text = txtHasil.Text & 6
+    End Sub
+
+    Private Sub btn7_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn7.Click
+        txtHasil.Text = txtHasil.Text & 7
+    End Sub
+
+    Private Sub btn8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn8.Click
+        txtHasil.Text = txtHasil.Text & 8
+    End Sub
+
+    Private Sub btn9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn9.Click
+        txtHasil.Text = txtHasil.Text & 9
+    End Sub
+
+    Private Sub btnBagi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBagi.Click
+        FirstNumber = Val(txtHasil.Text)
+        txtHasil.Text = "0"
+        ArithmeticProcess = "/"
+    End Sub
+
+    Private Sub btnKali_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnKali.Click
+        FirstNumber = Val(txtHasil.Text)
+        txtHasil.Text = "0"
+        ArithmeticProcess = "*"
+    End Sub
+
+    Private Sub btnKurang_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnKurang.Click
+        FirstNumber = Val(txtHasil.Text)
+        txtHasil.Text = "0"
+        ArithmeticProcess = "-"
+    End Sub
+
+    Private Sub btnTambah_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTambah.Click
+        FirstNumber = Val(txtHasil.Text)
+        txtHasil.Text = "0"
+        ArithmeticProcess = "+"
+    End Sub
+
+    Private Sub btnHasil_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHasil.Click
+        SecondNumber = Val(txtHasil.Text)
+        If ArithmeticProcess = "+" Then
+            AnswerNumber = FirstNumber + SecondNumber
+        End If
+        If ArithmeticProcess = "-" Then
+            AnswerNumber = FirstNumber - SecondNumber
+        End If
+        If ArithmeticProcess = "*" Then
+            AnswerNumber = FirstNumber * SecondNumber
+        End If
+        If ArithmeticProcess = "/" Then
+            AnswerNumber = FirstNumber / SecondNumber
+        End If
+        txtHasil.Text = AnswerNumber
+    End Sub
+
+    
+End Class
